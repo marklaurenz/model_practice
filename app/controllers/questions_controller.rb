598b8a61440e6d004c66fb1693b26644d1bce80c
Mark Laurenz <m-laurenz@kellogg.northwestern.edu>
2014-05-11 19:15:59 -0500

    Q2, Q3 and Q4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,27 +19,71 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
     # the_longest_movie_director_id = Movie.order("duration ASC").last.director_id
-    the_longest_movie_director_id = Director.where(Movie.order("duration ASC").last.director_id)
+    # the_longest_movie = Movie.order("duration ASC").last.director_id
 
-    @director_of_longest_movie = the_longest_movie_director_id.name
+    # the_director = Director.find(4)
+
+    @director_of_longest_movie = Movie.order("duration ASC").last.director.name
+
+    # where needs a column and criteria
+
+
 
   end
 
   def question_3
     # Which director has the most movies on the list?
+    # Name, max, number of movies per director .count, movies that belong to a director id
 
     # Your Ruby goes here.
 
-    # @director_with_the_most_movies = ???
+    # 1. check all movies for a director id and keep that information
+    # 2. Count the director ids for each director
+    # 3. Order the director ids from most to least (couldn't figure this out, instead replace next highest count as we go through the .each do)
+    # 4. Get the director's name with the most movies
+
+    # list_of_director_ids = Movie.count.director
+
+    the_director_with_the_most_movies = Director.new
+
+    Director.all.each do |the_director|
+      if the_director.movies.count > the_director_with_the_most_movies.movies.count
+        the_director_with_the_most_movies = the_director
+      end
+    end
+
+    @director_with_the_most_movies = the_director_with_the_most_movies.name
   end
 
   def question_4
     # Which actor has been in the most movies on the list?
     # (If there's a tie, any one of them is fine)
 
-    # Your Ruby goes here.
+    # 1. Check the list of roles
+    # 2. Count the number roles associated with each actor
+    # 3. The actor with the most roles should have the most movies
+    # roles_for_each_actor = []
 
-    # @actor_with_the_most_movies = ???
+    # Actor.all.each do |the_actor|
+    #   count_actors_roles = the_actor.roles.count
+
+    #   roles_for_each_actor.push(count_actors_roles)
+    # end
+
+
+    # @actor_with_the_most_movies = roles_for_each_actor.sort.last
+
+    the_leader = Actor.new
+
+    Actor.all.each do |the_actor|
+     if the_actor.movies.count > the_leader.movies.count
+       the_leader = the_actor
+     end
+   end
+
+  @actor_with_the_most_movies = the_leader.name
+
+    # @actor_with_the_most_movies = Actor.find(Role.count.max)
   end
 
   def question_5
--- app/models/director.rb
+++ app/models/director.rb
@@ -3,6 +3,7 @@ class Director < ActiveRecord::Base
 
   has_many :movies
 
+
   # def movies
   #   return Movie.where(:director_id => id)
   # end
